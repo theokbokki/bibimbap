57542cce09dfb6291e0c1f716aae8ba9b2efb3d3
&#10059 <90846584+theoleonet@users.noreply.github.com>
2023-06-22 15:19:01 +0200

    🎉 Initial commit
    Add the base files for the monorepo and a default Nuxt app with Tauri

---
+++ apps/desktop/src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="container">
+		<div class="content">
+			<h1>Bibimbap</h1>
+			<ui-btn>Click me!</ui-btn>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.container {
+	height: 100vh;
+	width: 100vw;
+	display: grid;
+	place-items: center;
+}
+
+.content {
+	text-align: center
+}
+
+h1 {
+	color: #999999;
+	font-family: sans-serif;
+}
+</style>
---
+++ packages/ui/lib/components/Btn.vue
@@ -0,0 +1,22 @@
+<template>
+	<button>
+		<slot></slot>
+	</button>
+</template>
+
+<style scoped>
+button {
+	border: none;
+	padding: .5rem 1rem;
+	background: #D590CC;
+	color: white;
+	font-size: 1.25rem;
+	font-family: sans-serif;
+	border-radius: 0.4375rem;
+}
+
+button:hover {
+	background: #C477B1;
+	cursor: pointer;
+}
+</style>
