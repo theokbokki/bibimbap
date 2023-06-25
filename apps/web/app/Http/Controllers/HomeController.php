f7bedc6954e235d7677539e7452c0750b4f1a244
&#10059 <90846584+theoleonet@users.noreply.github.com>
2023-06-25 11:37:49 +0200

    ✨ Add the Intertia app
    References #6
    
    This version will require JS as it assumes you chose to create a web-app that will most likely need JS to work.
    
    A no web-app no JS version will be added later on.

---
+++ apps/web/app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Response;
+use Inertia\ResponseFactory;
+
+class HomeController extends Controller
+{
+    public function index(): ResponseFactory|Response
+    {
+        return inertia('Home');
+    }
+}
---
+++ apps/web/resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Layout from '../Shared/Layout.vue'
+import { Head } from '@inertiajs/vue3'
+
+defineOptions({ layout: Layout })
+</script>
+
+<template>
+    <Head title="Home" />
+    <h1>Welcome</h1>
+</template>
