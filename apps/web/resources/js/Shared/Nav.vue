728f5a7dd83fa6747302808efb906e42b8b9cb7e
&#10059 <90846584+theoleonet@users.noreply.github.com>
2023-06-29 11:20:48 +0200

    💄 Add UI for the avatar modal
    References #5

--- apps/web/resources/js/Shared/Nav.vue
+++ apps/web/resources/js/Shared/Nav.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { usePage } from '@inertiajs/vue3'
 
 const user = computed(() => usePage().props.auth?.user)
+
+const avatarActionsElement = ref(null)
+let showAvatarActions = ref(false)
 </script>
 
 <template>
@@ -14,18 +17,28 @@ const user = computed(() => usePage().props.auth?.user)
         Bibimbap</Link>
 
         <!-- The User avatar -->
-        <div v-if="user" class="flex center-y | gx-16">
-            <p>@{{ user.username }}</p>
-            <Link class="avatar-image">
-            <img src="https://tinyurl.com/bibimbap-rickastley" alt="" class="circle" />
-            </Link>
+        <div v-if="user" class="grid | gy-8 | avatar" @click="showAvatarActions = !showAvatarActions">
+            <div class=" avatar-image">
+                <img src="https://tinyurl.com/bibimbap-rickastley" alt="" class="circle" />
+            </div>
+            <div ref="avatarActionsElement" class="grid | px-8 py-16 gy-16 radius-8 | avatar-actions"
+                :class="showAvatarActions ? 'shown' : 'hidden'">
+                <Link href="/profile" class="text-medium smooth | link no-underline">
+                View profile
+                </Link>
+                <div class=" pt-8 avatar-logout">
+                    <Link href="/logout" method="post" as="button" class="text-medium clr-error smooth pointer">
+                    Logout
+                    </Link>
+                </div>
+            </div>
         </div>
         <!-- The login et register links -->
         <div v-else-if="!$page.url.startsWith('/auth')" class=" flex center-y | gx-24">
             <Link href="/auth/login" class="text-medium-large smooth | link">
             Login
             </Link>
-            <Link as="button" type="submit" href="/auth/register" class="btn">
+            <Link href="/auth/register" class="btn">
             Register
             </Link>
         </div>
