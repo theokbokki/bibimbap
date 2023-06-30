5edaebc8c7e463f4fbe483f2388eeebf34ee18cd
&#10059 <90846584+theokbokki@users.noreply.github.com>
2023-06-30 11:13:26 +0200

    🛂 Add auth protections to routes
    Also added the index route
    
    References #5

---
+++ apps/web/resources/js/Pages/Index.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Layout from '../Shared/Layout.vue'
+import { Head } from '@inertiajs/vue3'
+import Infobox from '@bibimbap/ui/lib/components/Infobox.vue'
+
+defineOptions({ layout: Layout })
+</script>
+
+<template>
+    <Head title="Home page of the app" />
+
+    <section class="flex col flex-1 center-x center-y">
+        <p class="title-xl clr-neutral-400">An awesome app comes here!</p>
+    </section>
+</template>
