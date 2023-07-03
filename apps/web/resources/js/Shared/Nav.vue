8a81e3b354d87b305359741112d38cdf2cbff605
&#10059 <90846584+theokbokki@users.noreply.github.com>
2023-07-03 10:49:58 +0200

    🚚 Update links in Vue nav
    Also fixed some css bugs and a misplaced `"`
    
    References #13

--- apps/web/resources/js/Shared/Nav.vue
+++ apps/web/resources/js/Shared/Nav.vue
@@ -1,34 +1,29 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 import { usePage } from '@inertiajs/vue3'
 
 const user = computed(() => usePage().props.auth?.user)
-
-const avatarActionsElement = ref(null)
-let showAvatarActions = ref(false)
 </script>
 
 <template>
     <nav class="flex between center-y | mt-32 | nav">
         <h2 class="sr-only">Main nav</h2>
-        <a href="/home" class="title-small-medium"
-            :class="$page.url.startsWith('/auth') ? 'clr-neutral-200' : 'clr-primary-400'">
-            Bibimbap</a>
+        <a href="/" class="title-small-medium"
+            :class="$page.url.startsWith('/auth') ? 'clr-neutral-200' : 'clr-primary-400'">Bibimbap</a>
 
         <!-- The User avatar -->
-        <div v-if="user" class="grid | gy-8 | avatar" @click="showAvatarActions = !showAvatarActions">
-            <div class=" avatar-image">
+        <div v-if="user" class="grid | gy-8 | avatar">
+            <input type="checkbox" class="sr-only" id="avatar-toggle">
+            <label class="avatar-image pointer" for="avatar-toggle">
                 <img src="https://tinyurl.com/bibimbap-rickastley" alt="" class="circle" />
-            </div>
-            <div class="transparent-overlay" :class="showAvatarActions ? 'shown' : 'hidden'"
-                @click="showAvatarActions = !showAvatarActions"></div>
-            <div ref="avatarActionsElement" class="grid | px-8 py-16 gy-16 radius-8 | avatar-actions"
-                :class="showAvatarActions ? 'shown' : 'hidden'">
-                <Link href="/profile" class="text-medium smooth | link no-underline">
+            </label>
+            <label class="transparent-overlay" for="avatar-toggle"></label>
+            <div class="grid | px-8 py-16 gy-16 radius-8 | avatar-actions">
+                <Link href=" /profile" class="text-medium smooth | link no-underline">
                 View profile
                 </Link>
-                <div class=" pt-8 avatar-logout">
+                <div class="pt-8 avatar-logout">
                     <a href="/auth/login" method="delete" as="button" class="text-medium clr-error smooth pointer">
                         Logout
                     </a>
@@ -37,12 +32,12 @@ let showAvatarActions = ref(false)
         </div>
         <!-- The login et register links -->
         <div v-else-if="!$page.url.startsWith('/auth')" class=" flex center-y | gx-24">
-            <Link href="/auth/login" class="text-medium-large smooth | link">
-            Login
-            </Link>
-            <Link href="/auth/register" class="btn">
-            Register
-            </Link>
+            <a href="/auth/login" class="text-medium-large smooth | link">
+                Login
+            </a>
+            <a href="/auth/register" class="btn">
+                Register
+            </a>
         </div>
     </nav>
 </template>
