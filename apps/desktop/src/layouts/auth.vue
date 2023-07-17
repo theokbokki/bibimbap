d5556ffdb59e9a982ee98cc9991d2c5fa7eadd86
&#10059 <90846584+theokbokki@users.noreply.github.com>
2023-07-17 22:54:12 +0200

    💄 Add app base Vue UI
    References #2

---
+++ apps/desktop/src/layouts/auth.vue
@@ -0,0 +1,16 @@
+<template>
+	<NuxtLayout name="default">
+		<div class="grid | auth-page">
+			<div>
+				<div class="grid | bg-primary-200 | auth-filler">
+					<p class="ml-40 mb-40 title-large clr-neutral-200">Tauri apps with the power of Laravel</p>
+				</div>
+			</div>
+			<section class="grid center | gy-40 ml-32 | auth-form">
+				<slot>
+					<NuxtPage />
+				</slot>
+			</section>
+		</div>
+	</NuxtLayout>
+</template>
---
+++ apps/desktop/src/pages/auth/login.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import Field from '@bibimbap/ui/lib/components/Field.vue';
+
+useHead({
+	title: "Login page of Bibimbap",
+})
+definePageMeta({
+	layout: 'auth',
+});
+</script>
+
+<template>
+	<Head title="Login" />
+
+	<h3 class="title-medium-light text-center">Welcome back</h3>
+	<form @submit.prevent="" class="grid | gy-32">
+		<Field type="email" label="Email" id="email" name="email" :has-errors="false" placeholder="rick.astley@skynet.co">
+			<p v-if="false">{{ }}</p>
+		</Field>
+		<Field type="password" label="Password" id="password" name="password" :has-errors="false">
+			<p v-if="false">{{}}</p>
+		</Field>
+		<div class="grid | gy-16">
+			<button type="submit" class="btn" :disabled="false">Login</button>
+			<p class="text-end">Don’t have an account yet?
+				<NuxtLink to="http://web.test/auth/register" target="_blank" class="clr-primary-400 smooth | link">
+					Register
+				</NuxtLink>
+			</p>
+		</div>
+	</form>
+</template>
