8968887d9f1451447d699cf844e20186ad7c723a
&#10059 <90846584+theokbokki@users.noreply.github.com>
2023-07-26 09:21:00 +0200

    ♻️ Handle showing user avatar and logout
    References #21

--- packages/ui/lib/components/Nav.vue
+++ packages/ui/lib/components/Nav.vue
@@ -1,8 +1,10 @@
 <script setup>
 const url = useRequestURL()
-const user = null;
 
-async function handleSumbit() { console.log('logout') }
+async function handleSubmit() {
+	localStorage.removeItem('loggedIn');
+	navigateTo('/auth/login');
+}
 </script>
 
 <template>
@@ -12,7 +14,7 @@ async function handleSumbit() { console.log('logout') }
 			:class="url.pathname.startsWith('/auth') ? 'clr-neutral-200' : 'clr-primary-400'">Bibimbap</NuxtLink>
 
 		<!-- The User avatar -->
-		<div v-if="user" class="grid | gy-8 | avatar">
+		<div v-if="!url.pathname.startsWith('/auth')" class="grid | gy-8 | avatar">
 			<input type="checkbox" class="sr-only" id="avatar-toggle">
 			<label class="avatar-image pointer" for="avatar-toggle">
 				<img src="https://tinyurl.com/bibimbap-rickastley" alt="" class="circle" />
@@ -22,23 +24,12 @@ async function handleSumbit() { console.log('logout') }
 				<NuxtLink to="/profile" class="text-medium smooth | link no-underline">
 					View profile
 				</NuxtLink>
-				<div class="pt-8 avatar-logout">
-					<form @submit.prevent="handleSubmit" class="pt-8 | avatar-logout">
-						<button type="submit" class="text-medium clr-error smooth pointer">
-							Logout
-						</button>
-					</form>
-				</div>
+				<form @submit.prevent="handleSubmit" class="pt-8 | avatar-logout">
+					<button type="submit" class="text-medium clr-error smooth pointer">
+						Logout
+					</button>
+				</form>
 			</div>
-		</div>
-		<!-- The login et register links -->
-		<div v-else-if="!url.pathname.startsWith('/auth')" class="flex center-y | gx-24">
-			<NuxtLink to="/auth/login" class="text-medium-large smooth | link">
-				Login
-			</NuxtLink>
-			<NuxtLink to="http://web.test/auth/register" target="_blank" class="btn">
-				Register
-			</NuxtLink>
 		</div>
 	</nav>
 </template>
