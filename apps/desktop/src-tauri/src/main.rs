98e540b036ba384838e6045cb3b9e3c1ff27ff7f
&#10059 <90846584+theokbokki@users.noreply.github.com>
2023-07-28 14:53:33 +0200

    🛂 Add logic to check for valid token in deep link
    This is done to prevent someone from using their browser to trigger the deep link.
    Only the laravel app which knows the shared secret will be able to make a request
    
    References #20

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use shared_commands::check_jwt;
 use tauri::Manager;
 
 fn main() {
@@ -12,6 +13,22 @@ fn main() {
             let handle = app.handle();
             tauri_plugin_deep_link::register("bibimbap", move |request| {
                 dbg!(&request);
+
+                let token = request.split('?').nth(1).and_then(|query| {
+                    query
+                        .split('&')
+                        .find(|param| param.starts_with("token="))
+                        .map(|param| param.trim_start_matches("token="))
+                });
+
+                if let Some(token) = token {
+                    // Extract the path from the URL if the token is present
+                    let path = request.split("://").nth(1).unwrap_or_default();
+
+                    // Call the check_jwt function and debug its result
+                    dbg!(check_jwt(token.to_string()));
+                }
+
                 handle.emit_all("scheme-request-received", request).unwrap();
             })
             .unwrap();
