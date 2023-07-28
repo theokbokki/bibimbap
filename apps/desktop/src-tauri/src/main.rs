0ae20d2a431c55133117dd8bf33b57bc91a15636
&#10059 <90846584+theokbokki@users.noreply.github.com>
2023-07-28 12:13:37 +0200

    ✨ Make app and browser communicate
    Had to bundle the app first (for macos)
    Now I can send events to the app using the "bibimbap://" protocol
    
    References #20

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -1,9 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
+    tauri_plugin_deep_link::prepare("desktop.bibimbab.dev");
+
     #[cfg(debug_assertions)]
     tauri::Builder::default()
+        .setup(|app| {
+            let handle = app.handle();
+            tauri_plugin_deep_link::register("bibimbap", move |request| {
+                dbg!(&request);
+                handle.emit_all("scheme-request-received", request).unwrap();
+            })
+            .unwrap();
+
+            Ok(())
+        })
         .plugin(shared_commands::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
