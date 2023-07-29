7a307a810456c0f38139061a3e28ba6c4184d25f
&#10059 <90846584+theokbokki@users.noreply.github.com>
2023-07-29 11:53:30 +0200

    🛂 Add website registration logic
    It uses a combination of single use tokens (long random strings) and short lived jwt's to secure the communication between the app and the website
    
    References #20

--- apps/web/resources/views/auth/register.blade.php
+++ apps/web/resources/views/auth/register.blade.php
@@ -1,10 +1,13 @@
 <x-layouts.auth-layout title="Registration page of Bibimbap" head-title="Register">
     <h3 class="title-medium-light text-center">Welcome to Bibimbap</h3>
-    <form class="grid | gy-32" method="POST" action="/api/auth/register">
+    <form class="grid | gy-32" method="POST" action="/auth/register">
         @csrf
         <x-field name="username" label="Username" required placeholder="ahasucks" />
         <x-field name="email" label="Email" type="email" required placeholder="rick.astley@skynet.co" />
         <x-field name="password" label="Password" type="password" required />
+        @if ($token)
+            <input type="hidden" name="token" value={{ $token }} />
+        @endif
         <div class="grid | gy-16">
             <button type="submit" class="btn">Register</button>
             <p class="text-end">Already have an account?
--- packages/shared-commands/src/lib.rs
+++ packages/shared-commands/src/lib.rs
@@ -37,14 +37,14 @@ fn generate_jwt() -> String {
     token
 }
 
-pub fn check_jwt(jwt: String) -> bool {
+pub fn check_jwt(jwt: String) -> Result<bool, String> {
     match decode::<Claims>(
         &jwt,
         &DecodingKey::from_secret("secret".as_ref()),
         &Validation::default(),
     ) {
-        Ok(_) => true,
-        Err(_) => false,
+        Ok(_) => Ok(true),
+        Err(err) => Err(err.to_string()),
     }
 }
 
