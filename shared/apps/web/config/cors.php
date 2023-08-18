a65c740859cdae00daec954eac96997bda487ded
&#10059 <90846584+theokbokki@users.noreply.github.com>
2023-08-18 08:17:01 +0200

    🔗 Update link to use correct tld
    References #39

--- shared/apps/web/config/cors.php
+++ shared/apps/web/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://web.test'],
+    'allowed_origins' => ['http://web.localhost'],
 
     'allowed_origins_patterns' => [],
 
